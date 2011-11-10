6479ca05a44fd343b2cdafa3f738313cd81e411b
rowan.haigh@dfo-mpo.gc.ca <rowan.haigh@dfo-mpo.gc.ca@1f63469c-0564-11df-a9fb-49f3208c864b>
2011-11-10 18:54:29 +0000

    R-2.14.0 requires namespace and loading via '.onLoad'.

--- trunk/PBSddesolve/R/zzz.r
+++ trunk/PBSddesolve/R/zzz.r
@@ -1,13 +1,17 @@
-.First.lib <- function(lib,pkg)
+.onLoad <- function(lib,pkg)
 {
 	library.dynam("PBSddesolve", pkg, lib);
-	pkg_info <- utils::sessionInfo( package=pkg )$otherPkgs[[ pkg ]]
-	pkg_date <- strsplit( pkg_info$Packaged, " " )[[1]][1]
-
-	userguide_path <- system.file( "doc/PBSddesolve-UG.pdf", package = pkg )
+	pkg_info <- utils::sessionInfo( package="PBSddesolve" )$otherPkgs$PBSddesolve
+	if( is.character( pkg_info$Packaged ) )
+		pkg_date <- strsplit( pkg_info$Packaged, " " )[[1]][1]
+	else
+		pkg_date  <- "unkown"
+	
+	userguide_path <- system.file( "doc/PBSddesolve-UG.pdf", package = "PBSddesolve" )
 	
 	cat("
-PBSddesolve", pkg_info$Version, "-- Copyright (C) 2007-2010 Fisheries and Oceans Canada
+-----------------------------------------------------------
+PBSddesolve", pkg_info$Version, "-- Copyright (C) 2007-2011 Fisheries and Oceans Canada
 (based  on solv95 by Simon Wood)
 
 A complete user guide 'PBSddesolve-UG.pdf' is located at 
@@ -15,6 +19,7 @@ A complete user guide 'PBSddesolve-UG.pdf' is located at
 
 Packaged on", pkg_date, "
 Pacific Biological Station, Nanaimo
+-----------------------------------------------------------
 
 
 ")
